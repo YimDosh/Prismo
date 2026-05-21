566cc5d97a675fe85f772c28afd8efa2b4830fd8
Juan Martínez <jmartinez027.net@gmail.com>
2026-05-21 18:59:31 -0500

    Prismo-first-commit

---
+++ src/Prismo.Core/Features/Companies/Domain/Company.cs
@@ -0,0 +1,29 @@
+namespace Prismo.Core.Features.Companies.Domain;
+
+public class Company
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = null!;
+    public string Nit { get; private set; } = null!;
+
+    public bool IsActive { get; private set; } = true;
+    public DateTime CreatedAt { get; private set; }
+
+
+    private Company(){}
+
+    public static Company Create(string name, string nit)
+    { 
+        return new Company
+        {
+            Id = Guid.CreateVersion7(),
+            Name = name,
+            Nit = nit,
+            CreatedAt = DateTime.UtcNow
+        };
+        
+    }
+    public void Deactivate() => IsActive = false;
+    public void Activate() => IsActive = true;
+    
+}
