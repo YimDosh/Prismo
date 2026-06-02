4d9b9ae474c0b4dc611e0bde22f0ed32b3303d13
Juan Martínez <jmartinez027.net@gmail.com>
2026-06-02 18:02:37 -0500

    Migrations

---
+++ src/Prismo.Api/Endpoints/CreateCompanyEndpoint.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Prismo.Core.Features.Companies.CreateCompany;
+
+namespace Prismo.Api.Endpoints;
+
+public static class CreateCompanyEndpoint
+{
+    public static void MapCreateCompany(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/companies", async (CreateCompanyCommand command, ISender mediator, CancellationToken cancellationToken) =>
+        {
+            var response = await mediator.Send(command, cancellationToken);
+        }).WithName("CreateCompany");
+    }
+}
--- src/Prismo.Api/Program.cs
+++ src/Prismo.Api/Program.cs
@@ -1,19 +1,41 @@
+using Prismo.Api.Endpoints;
+using Scalar.AspNetCore;
+using Prismo.Core.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using Prismo.Core.Infrastructure.Common.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<PrismoDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Añade esto en la sección donde registras tus servicios (builder.Services)
+builder.Services.AddMediatR(cfg => 
+{
+    cfg.RegisterServicesFromAssembly(typeof(Prismo.Core.Features.Companies.UpdateCompany.UpdateCompanyHandler).Assembly);
+    
+    // 🔥 ESTA LÍNEA AGREGA EL VALIDADOR AL PIPELINE DE MEDIATR
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+// 🔥 ESTA LÍNEA REGISTRA TODOS LOS VALIDATORS (Como CreateCompanyValidator) DEL CORE
+builder.Services.AddValidatorsFromAssembly(typeof(Prismo.Core.Features.Companies.CreateCompany.CreateCompanyValidator).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
-
+app.MapCreateCompany();
 app.Run();
 
 
